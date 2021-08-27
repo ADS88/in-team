270925eeaf62164a542ddfec212b94e34d627987
Andrew Sturman <adsturmanator88@gmail.com>
2021-08-28 10:49:31 +1200

    Turned auth on for controllers

--- Server/Server.Api/Controllers/StudentController.cs
+++ Server/Server.Api/Controllers/StudentController.cs
@@ -12,7 +12,7 @@ namespace Server.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    // [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class StudentController : ControllerBase
     {
 
