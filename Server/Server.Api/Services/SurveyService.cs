47d37fc10f8e257756797c718f756bd2761c1eb3
Andrew Sturman <adsturmanator88@gmail.com>
2021-08-30 16:19:03 +1200

    Added filter to only get open surveys for student

--- Server/Server.Api/Services/SurveyService.cs
+++ Server/Server.Api/Services/SurveyService.cs
@@ -134,6 +134,7 @@ namespace Server.Api.Services
         public async Task<IEnumerable<Survey>> GetSurveysStudentNeedsToComplete(string userId){
             var user = await userRepository.GetUserWithTeams(userId);
             var surveysAssignedToStudent = await surveysRepository.GetSurveysAssignedToStudent(user);
+            surveysAssignedToStudent = surveysAssignedToStudent.Where(s => DateTimeOffset.Compare(s.ClosingDate, DateTimeOffset.UtcNow) < 0);
             var surveyAttemptsFromStudent = await surveysRepository.GetAttemptsFromUser(userId);
             var attemptedSurveyIds = new HashSet<int>(surveyAttemptsFromStudent.Select(s => s.Id));
             return surveysAssignedToStudent.Where(s => !attemptedSurveyIds.Contains(s.Id));
